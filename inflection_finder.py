52bd2249319c27770d7a4a2a017f644f4cce77da
Peter Conerly <pconerly@gmail.com>
2013-02-21 02:30:35 -0800

    did a first pass on revising code

--- inflection_finder.py
+++ inflection_finder.py
@@ -9,7 +9,9 @@ import shelve
 """
 An early step in any decipherment process: Is the language inflected?
 "walked" and "walking" are inflections of "walk", as is "dogs" of "dog".
-Won't catch irregular inflections like tener 'to have' inflected as tiene '(s)he has' and tengo 'i have' in Spanish, but is an organized step in approaching an undeciphered writing system
+Won't catch irregular inflections like tener 'to have' inflected as tiene 
+'(s)he has' and tengo 'i have' in Spanish, but is an organized step in 
+approaching an undeciphered writing system
 """
 
 # Phase I - kiminoa@gmail.com
@@ -17,69 +19,57 @@ Won't catch irregular inflections like tener 'to have' inflected as tiene '(s)he
 LOGLEVEL = "ERROR"
 CLUSTERED_FILE = "None"
 
-# Grab command line arguments
-OPTS, MISC = getopt.getopt(sys.argv[1:],"hf:l:",["help","file=","loglevel="])
+class CandidateFile:
+    def __init__(self, filename):
+        self.filename = filename
+        self.shelve = None
 
-def usage():
-    print "inflection_finder.py -f <csv-file> -l <loglevel:INFO|DEBUG|etc.>"
-    print "inflection_finder.py --file=<csv_file> --loglevel=DEBUG|INFO|WARNING|ERROR|CRITICAL"
+    def open(self):
+        if self.shelve == None:
+            self.shelve = shelve.open(self.filename)
 
-for opt, arg in OPTS:
-    if opt in ("-h", "--help"):
-        usage()
-        sys.exit(0)
-    elif opt in ("-f", "--file"):
-        CLUSTERED_FILE = arg
-    elif opt in ("-l", "--loglevel"):
-        loglevel = arg
-    else:
-        print "Unrecognized option."
-        usage()
-        sys.exit(2)
+    def close(self):
+        self.shelve.close()
+        self.shelve = None
 
-# Set up logging
-LOG = logging.getLogger(__name__)
-LOGLVL = getattr(logging, LOGLEVEL.upper()) # convert text log level to numeric
-LOG.setLevel(LOGLVL) # set log level
-HANDLER = logging.StreamHandler()
-LOG.addHandler(HANDLER)
 
-if CLUSTERED_FILE == "None":
-    sys.exit("File not specified.  Use -f or --file to specify; see inflection_finder.py -h.")
+    def add_candidate_to_file(self, root, inflections):
+        """
+        outputs to an interim file: each line is a key-value pair 
+        [ { 'root' : ['inflection', 'candidates'] } ]
+        """
+        root = root.encode('utf-8')
+        candidate_entry = [ {root : inflections } ]
+        LOG.debug("add_candidate_to_file: %s", candidate_entry)
+        
+        self.open()
+        self.shelve[root] = inflections
+        self.close()
 
-CANDIDATE_FILE = "candidate_inflections"
-
-def add_candidate_to_file(root, inflections):
-    """
-    outputs to an interim file: each line is a key-value pair [ { 'root' : ['inflection', 'candidates'] } ]
-    """
-    root = root.encode('utf-8')
-    candidate_entry = [ {root : inflections } ]
-    LOG.debug("add_candidate_to_file: %s", candidate_entry)
-	
-    cfile = shelve.open(CANDIDATE_FILE)
-    cfile[root] = inflections
-    cfile.close()
 
 def process_inflections(inflections):
     """
-    inputs a file with roots and inflection candidates, then determines which inflection candidates recur and are worth investigating
+    inputs a file with roots and inflection candidates, then determines 
+    which inflection candidates recur and are worth investigating
     
-    See also "Kober's Triplets", Alice Kober's early manual work on Linear B which used a similar methodology and was instrumental in the decipherment of Linear B.
+    See also "Kober's Triplets", Alice Kober's early manual work on Linear B 
+    which used a similar methodology and was instrumental in the decipherment 
+    of Linear B.
 
-    Phase I: Identify inflection candidates based on frequency of occurrence
+    Phase  I: Identify inflection candidates based on frequency of occurrence
     Phase II: Identify membership overlap between inflection candidates
     """
     # get the key-value pairs we stashed in shelve in process_clusters
     inflections = {}
     candidates = {}
     
-    cfile = shelve.open(CANDIDATE_FILE)
-    
+    cf.open()
+
     for x in cfile:
         LOG.debug("process_inflections: key from shelve: %s", x)
-        # each line contains a key-value pair [ {'root': ['list', 'of', 'inflection', 'candidates'] } ]
-        for inflection in cfile[x]:
+        # each line contains a key-value pair 
+        # [ {'root': ['list', 'of', 'inflection', 'candidates'] } ]
+        for inflection in cf.shelve[x]:
             # process through the value list and build our reverse key-value dictionary
             # each inflection candidate will act as a key and the roots as its list of values
             LOG.debug("process_inflections: inflection from shelve: %s", inflection)
@@ -87,13 +77,14 @@ def process_inflections(inflections):
                 inflections[inflection] = [] # initiate list
             inflections[inflection].append(x) # add root to candidate inflection
             
-    cfile.close()
-	
+    cf.close()
+    
     # now for the heavy-lifting.
     for i in inflections.keys():
         # special case: one instance
         if len(inflections[i]) == 1:
-            # if the inflection occurs only once, we can assume it's noise and throw it out until added data in a future run corroborates it
+            # if the inflection occurs only once, we can assume it's noise and 
+            # throw it out until added data in a future run corroborates it
             LOG.debug("process_inflections: nixing %s with only 1 instance in %s.", i, inflections[i])
             del inflections[i]
             continue
@@ -103,13 +94,15 @@ def process_inflections(inflections):
 
 def get_clusters(raw_file):
     """
-    [unimplemented] inputs a file of unique morphemes, outputs clusters of potential inflections
+    [unimplemented] inputs a file of unique morphemes, outputs clusters 
+    of potential inflections
     
-    Phase I: let OpenRefine do the heavy lifting and create the file of clusters (process_clusters) from raw data
+    Phase I: let OpenRefine do the heavy lifting and create the file of 
+    clusters (process_clusters) from raw data
     Phase II: use Python libraries to do the clustering (get_clusters)
     """
     pass
-	
+    
 def is_substring(substring, cluster):
     """
     inputs a substring to find, returns True only if found for all data in cluster
@@ -120,7 +113,7 @@ def is_substring(substring, cluster):
         is_found = is_found and substring in data
         LOG.debug("is_substring: is_found = %s\n", str(is_found))
     return is_found
-	
+    
 def longest_substring(cluster):
     """
     inputs a list of potential inflections, returns the longest common substring shared by all
@@ -139,7 +132,7 @@ def longest_substring(cluster):
                 LOG.debug("longest_substring: A substring match has been found: %s", substring)
     logging.info("Longest substring is %s.", substring)
     return substring
-	
+    
 def get_inflections(substring, cluster):
     """
     inputs the longest common substring for a cluster and returns the list of what *isn't* common in the cluster
@@ -167,9 +160,9 @@ def inflection_clusters(*args):
     """
     common_substring = longest_substring(*args)
     inflection_candidates = get_inflections(common_substring, *args)
-	
+    
     LOG.debug("Inflection candidates:\n%s", inflection_candidates)
-	
+    
     # Add common string as element 1 of list (treat as key) + inflections as element 2 of list
     add_candidate_to_file(common_substring, inflection_candidates)
 
@@ -186,8 +179,43 @@ def process_clusters(cluster_file):
         inflection_clusters(cluster_list) # discover inflection candidates for each morpheme list
         del cluster_list[:] # reinitialize for next cluster
     cfile.close()
-		
-# Let's do it.
-print "\nFiles with candidate clustered morphemes should be CSV (utf-8 is Ok)."				
-process_clusters(CLUSTERED_FILE)
-process_inflections(CANDIDATE_FILE)
+
+def usage():
+    print "inflection_finder.py -f <csv-file> -l <loglevel:INFO|DEBUG|etc.>"
+    print "inflection_finder.py --file=<csv_file> --loglevel=DEBUG|INFO|WARNING|ERROR|CRITICAL"
+
+
+if __name__ == "__main__":
+    # Grab command line arguments
+    OPTS, MISC = getopt.getopt(sys.argv[1:],"hf:l:",["help","file=","loglevel="])
+
+    for opt, arg in OPTS:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit(0)
+        elif opt in ("-f", "--file"):
+            CLUSTERED_FILE = arg
+        elif opt in ("-l", "--loglevel"):
+            loglevel = arg
+        else:
+            print "Unrecognized option."
+            usage()
+            sys.exit(2)
+
+    # Set up logging
+    LOG = logging.getLogger(__name__)
+    LOGLVL = getattr(logging, LOGLEVEL.upper()) # convert text log level to numeric
+    LOG.setLevel(LOGLVL) # set log level
+    HANDLER = logging.StreamHandler()
+    LOG.addHandler(HANDLER)
+
+    if CLUSTERED_FILE == "None":
+        sys.exit("File not specified.  Use -f or --file to specify; see inflection_finder.py -h.")
+
+    CANDIDATE_FILE = "candidate_inflections"
+    cf = CandidateFile()
+
+    # Let's do it.
+    print "\nFiles with candidate clustered morphemes should be CSV (utf-8 is Ok)."             
+    process_clusters(CLUSTERED_FILE)
+    process_inflections(CANDIDATE_FILE)
